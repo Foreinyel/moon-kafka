f6b46470f7b7112d4f9520492776c6e6e78813af
Foreinyel <Foreinyel@163.com>
2022-05-13 21:50:48 +0800

    default config

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,8 @@
-'use strict';
+"use strict";
 
 /**
  * egg-moon-kafka default config
  * @member Config#moonKafka
  * @property {String} SOME_KEY - some description
  */
-exports.moonKafka = {
-
-};
+exports.kafka = {};
